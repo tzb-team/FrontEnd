58f02a5de2364d3a7705e9eff8ec8bb794a3f266
“171250618” <171250618@smail.nju.edu.cn>
2019-02-28 11:43:00 +0800

    修改注册页面样式&增加信息验证响应

--- src/pages/signup.vue
+++ src/pages/signup.vue
@@ -4,9 +4,10 @@
     <div class="mainbox">
       <div class="mainbody">
         <h3 style="color: white;float: top;" align="center">注册</h3>
-        <input id="phone" type="text" style="margin-top: 20px;" placeholder="请输入手机号码">
+        <input id="account" type="text" style="margin-top: 20px;" placeholder="请输入用户名">
         <input id="email" type="text" style="margin-top: 20px" placeholder="请输入邮箱地址">
-        <input id="account" type="text" style="margin-top: 20px;" align="center" placeholder="请输入用户名，长度在2到15个字符之间"><br>
+        <input id="realName" type="text" style="margin-top: 20px;" align="center" placeholder="请输入真实姓名"><br>
+        <input id="IDcard" type="text" style="margin-top: 20px;" align="center" placeholder="请输入身份证号"><br>
         <input id="password" type="password" style="margin-top: 20px;" align="center" placeholder="请输入密码，长度在6到20个字符之间"><br>
         <input id="passwordConfirm" type="password" style="margin-top: 20px;" align="center" placeholder="确认密码"><br>
         <button id="signup" class="bigButton" v-on:click="signup()">注册</button><br>
@@ -25,12 +26,123 @@
         name: "signup",
       components: {footerBar},
       mounted: function(){
-
+        $("#account").change(function () {
+          if($("#account").val().length<2){
+            alert("用户名长度过短");
+            $('#signup').attr("disabled",true);
+            this.focus();
+          }
+          else if($("#account").val().length>15){
+            alert("用户名长度过长");
+            $('#signup').attr("disabled",true);
+            this.focus();
+          }
+          else{
+            $('#signup').attr("disabled",false);
+          }
+        });
+        $("#password").change(function () {
+          if($("#password").val().length<6){
+            alert("密码长度过短");
+            $('#signup').attr("disabled",true);
+            this.focus();
+          }
+          else if($("#password").val().length>20){
+            alert("密码长度过长");
+            $('#signup').attr("disabled",true);
+            this.focus();
+          }
+          else{
+            $('#signup').attr("disabled",false);
+          }
+        });
+        $("#passwordConfirm").change(function () {
+          if($("#passwordConfirm").val()!=$("#password").val()){
+            alert("密码前后不一致");
+            $('#signup').attr("disabled",true);
+            this.focus();
+          }
+          else{
+            $('#signup').attr("disabled",false);
+          }
+        });
       },
 
       methods:{
           signup: function () {
+            var ac=$('#account').val();
+            var pw=$('#password').val();
+            var phone=$('#phone').val();
+            var email=$('#email').val();
+            var mc=$('#messageCode').val();
+            var ec=$('#emailCode').val();
+            this.$axios.post("http://localhost:8000/api/auth/signup", {"account": ac, "password": pw,"phone":phone,"email":email,"messageCode":mc,"emailCode":ec}).then(res => {
+              var data=res.data;
+              if(data.result==1){
+                alert("注册成功!");
+                this.$router.replace('/login');
+              }else if(data.result==2){
+                alert("用户名重复，请另取");
+                $('account').focus();
+              }else if(data.result==3){
+                alert("验证码错误");
+              }else{
+                alert("请重试");
+              }
 
+            });
+          },
+          sendEmail: function () {
+            var email=$('#email').val();
+            this.$axios.post("http://localhost:8000/api/auth/signup/sendEmail", {"email":email}).then(res => {
+              var data=res.data;
+              if(data.result==1){
+                $('#sendEmail').attr("disabled",true);
+                var time=60;
+                var myScroll = setInterval(() => {
+                  time--;
+                  if(time>=0) {
+                    $('#sendEmail').html(time + "s后重发送");
+                  }else{
+                    $('#sendEmail').html("发送验证码");
+                    $('#sendEmail').attr("disabled",false);   //倒计时结束能够重新点击发送的按钮
+                    clearTimeout(timer);    //清除定时器
+                    time = 60;   //设置循环重新开始条件
+                  }
+                }, 1000);
+              }else if(data.result==2){
+                alert("邮箱地址有误");
+              }else{
+                alert("发送失败");
+              }
+
+            });
+          },
+          sendMessage: function () {
+            var phone=$('#phone').val();
+            this.$axios.post("http://localhost:8000/api/auth/signup/sendMessage", {"phone":phone}).then(res => {
+              var data=res.data;
+              if(data.result==1){
+                $('#sendMessage').attr("disabled",true);
+                var time=60;
+                var myScroll = setInterval(() => {
+                  time--;
+                  if(time>=0) {
+                    $('#sendMessage').html(time + "s后重发送");
+                  }else{
+                    $('#sendMessage').html("发送验证码");
+                    $('#sendMessage').attr("disabled",false);   //倒计时结束能够重新点击发送的按钮
+                    clearTimeout(timer);    //清除定时器
+                    time = 60;   //设置循环重新开始条件
+                  }
+                }, 1000);
+              }else if(data.result==2){
+                alert("手机号码有误");
+              }else{
+                alert("发送失败");
+              }
+
+            });
           }
       }
 
