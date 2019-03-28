e3bdfc05c67e5f8d8cf64bdcb8434812547459e5
JunboXu <824365307@qq.com>
2019-03-28 16:51:46 +0800

    专利池展示

--- src/pages/patentSetDetails.vue
+++ src/pages/patentSetDetails.vue
@@ -5,72 +5,40 @@
       <el-submenu index="1">
         <template slot="title"><i class="el-icon-message">导航</i></template>
         <el-menu-item-group>
-          <!-- <template slot="title">分组一</template> -->
+
           <el-menu-item index="1-1">池中专利</el-menu-item>
-          <el-menu-item index="1-2">池中联系人</el-menu-item>
+          
         </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="1-3">选项3</el-menu-item>
+        <el-menu-item-group title="">
+
         </el-menu-item-group>
-        <el-submenu index="1-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="1-4-1">选项4-1</el-menu-item>
-        </el-submenu>
+       
       </el-submenu>
-      <el-submenu index="2">
-        <template slot="title"><i class="el-icon-menu"></i>导航二</template>
-        <el-menu-item-group>
-          <template slot="title">分组一</template>
-          <el-menu-item index="2-1">选项1</el-menu-item>
-          <el-menu-item index="2-2">选项2</el-menu-item>
-        <!-- </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="2-3">选项3</el-menu-item>
-        </el-menu-item-group> -->
-        <!-- <el-submenu index="2-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="2-4-1">选项4-1</el-menu-item>
-        </el-submenu> -->
-        </el-menu-item-group>
-      </el-submenu>
-      <el-submenu index="3">
-        <template slot="title"><i class="el-icon-setting"></i>导航三</template>
-        <el-menu-item-group>
-          <template slot="title">分组一</template>
-          <el-menu-item index="3-1">选项1</el-menu-item>
-          <el-menu-item index="3-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="3-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="3-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="3-4-1">选项4-1</el-menu-item>
-        </el-submenu>
-      </el-submenu>
+    
+     
     </el-menu>
   </el-aside>
-  
+
   <el-container>
     <el-header style="text-align: right; font-size: 12px">
-      <!-- <el-dropdown>
-        <i class="el-icon-setting" style="margin-right: 15px"></i>
-        <el-dropdown-menu slot="dropdown">
-          <el-dropdown-item>查看</el-dropdown-item>
-          <el-dropdown-item>新增</el-dropdown-item>
-          <el-dropdown-item>删除</el-dropdown-item>
-        </el-dropdown-menu>
-      </el-dropdown>
-      <span>王小虎</span> -->
+
     </el-header>
     <el-main>
       <el-table :data="tableData">
-        <el-table-column prop="dateJoin" label="入池日期" width="140">
-        </el-table-column>
-        <el-table-column prop="patentname" label="专利名称" width="120">
+        <el-table-column prop="patentname" label="专利名称">
         </el-table-column>
         <el-table-column prop="patentid" label="专利id">
         </el-table-column>
+        <el-table-column prop="type" label="专利所属行业" >
+        </el-table-column>
+        <el-table-column prop="dateJoin" label="入池日期" >
+        </el-table-column>
+         <el-table-column prop="dateexit" label="出池日期">
+        </el-table-column>
+        <el-table-column prop="ownerid" label="专利持有人">
+        </el-table-column>
+        
+    
       </el-table>
     </el-main>
   </el-container>
@@ -86,6 +54,7 @@
   .el-aside {
     color: #333;
   }
+
 </style>
 
 <script>
@@ -94,10 +63,13 @@
       const item = {
         dateJoin: '2016-05-02',
         patentname: '时光传送机',
-        patentid: '0000000001'
+        type:'信息技术',
+        dateexit:'无',
+        patentid: '0000000001',
+        ownerid: '222222'
       };
       return {
-        tableData: Array(20).fill(item)
+        tableData: Array(6).fill(item)
       }
     }
   };
